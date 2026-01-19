f3d5ee03fb2717230b5043d4e415c30ca0e6770f
Sarju88 <1093459@lwsd.org>
2026-01-19 15:29:12 -0800

    Hopefully fixed controls

--- scripts/mobile_controls.gd
+++ scripts/mobile_controls.gd
@@ -1,6 +1,7 @@
 extends CanvasLayer
 
 @export var show_on_desktop: bool = false
+@export var enable_web_pointer_lock: bool = true
 
 func _ready() -> void:
 	var is_touch := DisplayServer.is_touchscreen_available()
@@ -16,6 +17,18 @@ func _ready() -> void:
 				continue
 			node.button_down.connect(_on_button_down.bind(action_name))
 			node.button_up.connect(_on_button_up.bind(action_name))
+
+func _input(event: InputEvent) -> void:
+	if not enable_web_pointer_lock:
+		return
+	if not OS.has_feature("web"):
+		return
+	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		return
+	if event is InputEventMouseButton and event.pressed:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	elif event is InputEventScreenTouch and event.pressed:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _on_button_down(action_name: String) -> void:
 	Input.action_press(action_name)
